158b456afd0e2ce462029d714322dcabd9a5e5b5
sudohorus <sudohorusdev@gmail.com>
2025-10-03 12:11:17 -0300

    7. Reverse Integer

---
+++ 7. Reverse Integer/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int r = 0;
+        while(x != 0){
+            int d = x % 10;
+
+            if((r > INT_MAX / 10) || (r < INT_MIN / 10)){
+                return 0;
+            }
+            r = r * 10 + d;
+            x = x / 10;
+        }
+        return r;
+    }
+};
